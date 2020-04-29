d5202109f6fb0c1b595fd1ffbb8c16c8dc85db70
rui4ymipat <Peerapatpatty@gmail.com>
2020-04-29 14:53:24 +0700

    Res web

--- production/history.php
+++ production/history.php
@@ -52,74 +52,32 @@
       font-size: 60%;
     }
 
-    .column1 {
-      width: 57%;
 
-    }
 
     td.name {
       font-size: 140%;
       padding-right: 20px;
       padding-top: 1%;
+    }
+    @media only screen and (max-width: 1366px) {
+      .column1 {
+        width: 56%;
+      }
+    }
+
+    @media only screen and (max-width: 1024px) {
+      .column1 {
+        width: 85%;
+      }
 
     }
-    @media only screen and (min-width: 1200px) { 
-   .col-xl-1 { width: 8.33% }
-   .col-xl-2 { width: 16.66% }  
-   .col-xl-3 { width: 25% }
-   .col-xl-4 { width: 33.33% }
-   .col-xl-5 { width: 41.66% }
-   .col-xl-6 { width: 50% }
-   .col-xl-7 { width: 58.33% }
-   .col-xl-8 { width: 66/66% }
-   .col-xl-9 { width: 75% }
-   .col-xl-10 { width: 83.33% }
-   .col-xl-11 { width: 91.66% }
-   .col-xl-12 { width: 100% }
-}
-@media only screen and (min-width: 992px) { 
-   .col-xl-1 { width: 8.33% }
-   .col-xl-2 { width: 16.66% }  
-   .col-xl-3 { width: 25% }
-   .col-xl-4 { width: 33.33% }
-   .col-xl-5 { width: 41.66% }
-   .col-xl-6 { width: 50% }
-   .col-xl-7 { width: 58.33% }
-   .col-xl-8 { width: 66/66% }
-   .col-xl-9 { width: 75% }
-   .col-xl-10 { width: 83.33% }
-   .col-xl-11 { width: 91.66% }
-   .col-xl-12 { width: 100% }
-}
 
-@media only screen and (min-width: 768px) { 
-   .col-xl-1 { width: 8.33% }
-   .col-xl-2 { width: 16.66% }  
-   .col-xl-3 { width: 25% }
-   .col-xl-4 { width: 33.33% }
-   .col-xl-5 { width: 41.66% }
-   .col-xl-6 { width: 50% }
-   .col-xl-7 { width: 58.33% }
-   .col-xl-8 { width: 66/66% }
-   .col-xl-9 { width: 75% }
-   .col-xl-10 { width: 83.33% }
-   .col-xl-11 { width: 91.66% }
-   .col-xl-12 { width: 100% }
-}
-@media only screen and (min-width: 600px) { 
-   .col-xl-1 { width: 8.33% }
-   .col-xl-2 { width: 16.66% }  
-   .col-xl-3 { width: 25% }
-   .col-xl-4 { width: 33.33% }
-   .col-xl-5 { width: 41.66% }
-   .col-xl-6 { width: 50% }
-   .col-xl-7 { width: 58.33% }
-   .col-xl-8 { width: 66/66% }
-   .col-xl-9 { width: 75% }
-   .col-xl-10 { width: 83.33% }
-   .col-xl-11 { width: 91.66% }
-   .col-xl-12 { width: 100% }
-}
+    @media only screen and (min-width: 1024px) {
+      .column1 {
+        width: 33%;
+      }
+
+    }
   </style>
   <?php
   session_start();
