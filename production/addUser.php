0ee3e68c8190010f75642869f7779b415e6d00d4
rui4ymipat <Peerapatpatty@gmail.com>
2020-04-29 21:10:43 +0700

    management.php

--- production/addUser.php
+++ production/addUser.php
@@ -11,43 +11,36 @@ $id = $_POST['id'];
 $pass = $_POST['pass'];
 $Fname = $_POST['Fname'];
 $Lname = $_POST['Lname'];
-if(empty($_POST['id']))
-{
+if (empty($_POST['id'])) {
     $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ กรุณาลองใหม่อีกครั้ง';
     header("Location:management.php");
-}
-else if(empty($_POST['pass']))
-{
+} else if (empty($_POST['pass'])) {
     $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ กรุณาลองใหม่อีกครั้ง';
     header("Location:management.php");
-}
-else if(empty($_POST['Lname']))
-{
+} else if (empty($_POST['Lname'])) {
     $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ กรุณาลองใหม่อีกครั้ง';
     header("Location:management.php");
-}
-else if(empty($_POST['Fname']))
-{
+} else if (empty($_POST['Fname'])) {
     $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ กรุณาลองใหม่อีกครั้ง';
     header("Location:management.php");
-}
-else
-{
+} else {
+    $add = TRUE;
     for ($i = 0; $i < count($username); $i++) {
         if ($id == $username[$i]) {
             $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ ID ถูกใช้ไปแล้ว';
+            $add = FALSE;
             header("Location:management.php");
         }
     }
-    
-    
-    $userQuery = "INSERT INTO user(Fname,Lname,username,password) VALUES('$Fname','$Lname','$id','$pass')";
-    $result = mysqli_query($connect, $userQuery);
-    if (!$result) {
-        $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ กรุณาลองใหม่อีกครั้ง';
-        header("Location:management.php");
-    } else {
-        $_SESSION['msg'] = 'การดำเนินการสำเร็จ';
-        header("Location:management.php");
+    if ($add) {
+        $userQuery = "INSERT INTO user(Fname,Lname,username,password) VALUES('$Fname','$Lname','$id','$pass')";
+        $result = mysqli_query($connect, $userQuery);
+        if (!$result) {
+            $_SESSION['msg'] = 'การดำเนินการไม่สำเร็จ กรุณาลองใหม่อีกครั้ง';
+            header("Location:management.php");
+        } else {
+            $_SESSION['msg'] = 'การดำเนินการสำเร็จ';
+            header("Location:management.php");
+        }
     }
 }
