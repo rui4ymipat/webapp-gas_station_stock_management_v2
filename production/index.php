5e312784d9b6230c8754a47d9fc83352ae2453e0
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-07 04:01:11 +0700

    C fuelprice

--- production/index.php
+++ production/index.php
@@ -469,13 +469,13 @@
                   <h1 class="todayprice">ยอดขายย้อนหลัง 15 วัน</h1>
                 
                 <br>
-                <h1 style="font-size: 150%;">แก็สโซฮอล์ 91</h1>
+                <h1 style="font-size: 150%; padding-left: 26%;">แก็สโซฮอล์ 91</h1>
                 <div id="G91" style="width: 1000px; height: 400px;"></div>
                 <br>
-                <h1 style="font-size: 150%;">แก็สโซฮอล์ 95</h1>
+                <h1 style="font-size: 150%;padding-left: 26%;">แก็สโซฮอล์ 95</h1>
                 <div id="G95" style="width: 1000px; height: 400px;"></div>
                 <br>
-                <h1 style="font-size: 150%;">ดีเซล</h1>
+                <h1 style="font-size: 150%; padding-left: 29%;">ดีเซล</h1>
                 <div id="De" style="width: 1000px; height: 400px;"></div>
                 </div>
 
