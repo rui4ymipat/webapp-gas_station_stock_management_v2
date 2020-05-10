5eee41d62e8dd6fd3a1a0b784ef8629a9bd562fe
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-11 00:12:41 +0700

    back end

--- production/fuelprice.php
+++ production/fuelprice.php
@@ -272,7 +272,17 @@
                 <div class="column1">
                   <h1 style="font-weight: 800;color: #2B3E54;font-size: 180%;">กรอกข้อมูลประจำวัน</h1>
                   <div class="setborder">
-
+                    <?php
+                    if (!empty($_SESSION['msg'])) {
+                      session_start();
+                    ?>
+                      <script type="text/javascript">
+                        alert('<?php echo $_SESSION['msg'] ?>');
+                      </script>
+                    <?php
+                      unset($_SESSION['msg']);
+                    }
+                    ?>
                     <?php
                     if (!empty($_SESSION['msg'])) {
                       session_start();
