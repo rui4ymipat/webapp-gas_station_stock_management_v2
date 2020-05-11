e87189b230432dc96d418d2673eab5abdbaa649a
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-12 00:20:28 +0700

    C table

--- production/add_history.php
+++ production/add_history.php
@@ -23,7 +23,8 @@
     $a3_de_p = $_GET['Dep_a3'];
     $a3_de_v = $_GET['Ded_a3'];
     $car = $_GET['cars'];
-    if(empty($a1_91_p) && empty($a1_91_v) && empty($a1_95_p) && empty($a1_95_v) && empty($a1_de_p) && empty($a1_de_v) && empty($a2_91_p)  && empty($a2_91_v) && empty($a2_95_p) && empty($a2_95_v) && empty($a2_de_p)  && empty($a2_de_v) && empty($a3_91_p) && empty($a3_91_v) && empty($a3_91_d) && empty($a3_91_v) && empty($a3_95_p) && empty($a3_95_v) && empty($a3_de_p)&& empty($a3_de_v))
+    $tran = $_GET['a'];
+    if(empty($a) && empty($a1_91_p) && empty($a1_91_v) && empty($a1_95_p) && empty($a1_95_v) && empty($a1_de_p) && empty($a1_de_v) && empty($a2_91_p)  && empty($a2_91_v) && empty($a2_95_p) && empty($a2_95_v) && empty($a2_de_p)  && empty($a2_de_v) && empty($a3_91_p) && empty($a3_91_v) && empty($a3_91_d) && empty($a3_91_v) && empty($a3_95_p) && empty($a3_95_v) && empty($a3_de_p)&& empty($a3_de_v))
     {
         $_SESSION['msg'] = "โปรดกรอกข้อมูลให้ครบ";
         header("Location:history.php"); 
@@ -33,7 +34,7 @@
     $date = date("Y-m-d");
     $time = date("h:i:sa");
     $name = $_SESSION['user_name'];
-    $tran = $_GET['a'];
+    
     //account 1
     $Qa1="insert into history(car_id,gas_id,price,value,tran_price,account,date,timestamp,name_stamp) 
             values($car,1,$a1_91_p,$a1_91_v,$tran,1,'$date','$time','$name')";
--- production/fuelprice.php
+++ production/fuelprice.php
@@ -382,8 +382,7 @@
                         <td>2</td>
                         <td>3</td>
                         <td>4</td>
-                        <td>5</td>
-                       
+                        <td>5</td>                   
                       </tr>
                     </table>
                   </div>
