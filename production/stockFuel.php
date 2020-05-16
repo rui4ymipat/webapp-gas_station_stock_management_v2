e2b8cd9e84dd60e8d4dafbd9cce74dd388f865d1
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-17 03:22:32 +0700

    C

--- production/stockFuel.php
+++ production/stockFuel.php
@@ -431,6 +431,19 @@
                         <th>G95</th>
                         <th>Desel</th>
                       </tr>
+                      <tr>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                        <td>8</td>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                      </tr>
                       <?php
                       date_default_timezone_set("Asia/Bangkok");
                       $d = date("Y-m-d");
