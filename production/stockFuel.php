7b9815cca98d08db0f7f020a8507be75fc1d2f5a
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-17 03:16:24 +0700

    C

--- production/stockFuel.php
+++ production/stockFuel.php
@@ -463,6 +463,7 @@
                           ?>
                               <td><?php echo number_format($row['value']); ?></td>
                               <?php
+                              }
                               $userQuery = "select * from gas where date = '$da' and substr(timestamp,9) = '$f_time' order by gas_id";
                               $result = mysqli_query($connect, $userQuery);
                               $row = mysqli_fetch_assoc($result);
@@ -490,7 +491,7 @@
                               <td><?php echo $row['diesel']  ?></td>
                         </tr>
                     <?php
-                            }
+                            
                           } ?>
                     </table>
                     <table border="1" style="display: inline-block; margin-left:2%;text-align: center;width:25%;">
