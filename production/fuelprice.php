d423dbb1fdc3f749e9b95501c1f615bfca32455f
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-17 15:44:40 +0700

    C

--- production/fuelprice.php
+++ production/fuelprice.php
@@ -436,21 +436,21 @@
                       date_default_timezone_set("Asia/Bangkok");
                       $setDateStart = date("Y-m-01");
                       $setDateEnd = date("Y-m-31");
-                      $userQuery = "SELECT * from sell where account = 1 order by date DESC limit 7";
+                      $userQuery = "SELECT * from sell where account = 1 order by date DESC limit 21";
                       $result = mysqli_query($connect, $userQuery);
                       while ($row = mysqli_fetch_assoc($result)) {
                         $a1_price[] = $row['price'];
                         $a1_discount[] = $row['discount'];
                       }
 
-                      $userQuery = "SELECT * from sell where account = 2 order by date DESC limit 7";
+                      $userQuery = "SELECT * from sell where account = 2 order by date DESC limit 21";
                       $result = mysqli_query($connect, $userQuery);
                       while ($row = mysqli_fetch_assoc($result)) {
                         $a2_price[] = $row['price'];
                         $a2_discount[] = $row['discount'];
                       }
 
-                      $userQuery = "SELECT * from sell where account = 3 order by date DESC limit 7";
+                      $userQuery = "SELECT * from sell where account = 3 order by date DESC limit 21";
                       $result = mysqli_query($connect, $userQuery);
                       while ($row = mysqli_fetch_assoc($result)) {
                         $a3_price[] = $row['price'];
