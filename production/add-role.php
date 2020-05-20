e95e04f7a7aa7b523889bd854af66cd4ad537f83
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-21 03:37:38 +0700

    C

--- production/add-role.php
+++ production/add-role.php
@@ -3,7 +3,7 @@ session_start();
  $t = $_GET['t'];
  $file = fopen('note.txt','w');
  $text= str_replace("\n", "<br>\n", $t);
- fprintf($file,"%s",$text);
+ fwrite($file,$text);
  fclose($file);
  $_SESSION['msg'] = "ดำเนินการสำเร็จ";
  header("Location:management.php");
