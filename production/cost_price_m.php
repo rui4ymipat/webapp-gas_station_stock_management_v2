c3572d71d297e707fd5aa12074497390199bfe21
rui4ymipat <Peerapatpatty@gmail.com>
2020-05-23 02:07:13 +0700

    C

--- production/cost_price_m.php
+++ production/cost_price_m.php
@@ -156,7 +156,7 @@
 
 
               <div class="setfont1">
-                <h1 style="font-weight: 800;color: #2B3E54;font-size: 180%;">กรอกข้อมูลประจำวัน</h1>
+                <h1 style="font-weight: 800;color: #2B3E54;font-size: 180%;">ต้นทุน</h1>
                 <form action="cost_price_m.php" method="GET">
                   <div class="qwer">
                     <table class="asdasd">
