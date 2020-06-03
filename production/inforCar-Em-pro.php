aff6e6d4bdb1ff4c019d23452a8bee4bbb2b3adc
rui4ymipat <Peerapatpatty@gmail.com>
2020-06-03 12:04:47 +0700

    C

--- production/inforCar-Em-pro.php
+++ production/inforCar-Em-pro.php
@@ -4,7 +4,7 @@
     date_default_timezone_set("Asia/Bangkok");
     function check($a){
         if(empty($a)){
-            return 0;
+            return (int)0;
         }
         else{
             return $a;
