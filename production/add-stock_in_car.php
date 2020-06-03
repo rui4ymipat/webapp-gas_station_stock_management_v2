5da298094dfbe92ebed57def55aa269443e6542b
rui4ymipat <Peerapatpatty@gmail.com>
2020-06-03 11:47:51 +0700

    C

--- production/add-stock_in_car.php
+++ production/add-stock_in_car.php
@@ -8,16 +8,52 @@ $f_time = substr($time,8);
 $name = $_SESSION['user_name'];
 
 $g91_6134 = $_GET['6134g91'];
+if(empty($g91_6134))
+{
+    $g91_6134 = 0;
+}
 $g95_6134 = $_GET['6134g95'];
+if(empty($g95_6134))
+{
+    $g95_6134 = 0;
+}
 $de_6134 = $_GET['6134de'];
+if(empty($de_6134))
+{
+    $de_6134 = 0;
+}
 
 $g91_1815 = $_GET['1815g91'];
+if(empty($g91_1815))
+{
+    $g91_1815 = 0;
+}
 $g95_1815 = $_GET['1815g95'];
+if(empty($g95_1815))
+{
+    $g95_1815 = 0;
+}
 $de_1815 = $_GET['1815de'];
+if(empty($de_1815))
+{
+    $de_1815 = 0;
+}
 
 $g91_is = $_GET['isg91'];
+if(empty($g91_is))
+{
+    $g91_is = 0;
+}
 $g95_is = $_GET['isg95'];
+if(empty($g95_is))
+{
+    $g95_is = 0; 
+}
 $de_is = $_GET['isde'];
+if(empty($de_is))
+{
+    $de_is = 0;
+}
 
 $userQuery = "select * from gas6134 where date = '$date'";
 $result = mysqli_query($connect, $userQuery);
@@ -51,6 +87,7 @@ if (mysqli_num_rows($result) == 0) {
 } else {
     $userQuery = "update gasisuzu set g91 = $g91_is, g95 = $g95_is, diesel = $de_is  , name_stamp = '$name' ,timestamp = '$time' where date = '$date'";
     $result = mysqli_query($connect, $userQuery);
+
 }
 $_SESSION['msg'] = "ดำเนินการสำเร็จ";
 header("Location:stockFuel.php");
